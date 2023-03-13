7f6e199cb57d11ab7cdd6128b949aa137dbd5093
tomatoYum <tomato6726@gmail.com>
2023-03-13 14:55:56 +0900

    file structure setting 🔧

---
+++ src/test/java/com/temp/kelog/KelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temp.kelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
